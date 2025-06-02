72086d40d31ca3e7225da6b003a7db102d4198d7
PCMchatgtp <fufumaison@gmail.com>
2025-06-02 19:46:12 +0200

    config.py

--- config.py
+++ config.py
@@ -3,5 +3,8 @@ import os
 TOKEN = os.getenv("TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
 
+print("TOKEN =", TOKEN)
+print("CHAT_ID =", CHAT_ID)
+
 if not TOKEN or not CHAT_ID:
     raise ValueError("❌ TOKEN ou CHAT_ID manquant dans Render.")
