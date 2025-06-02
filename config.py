7234545195aadaa30939b85cb200926f6bcf4725
PCMchatgtp <fufumaison@gmail.com>
2025-06-02 19:39:21 +0200

    config.py

--- config.py
+++ config.py
@@ -1,9 +1,7 @@
 import os
 
-# Récupération des variables d'environnement depuis Render
 TOKEN = os.getenv("TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
 
-# Vérification (optionnel, utile pour debug)
-if TOKEN is None or CHAT_ID is None:
-    raise ValueError("❌ Les variables TOKEN ou CHAT_ID ne sont pas définies dans Render.")
+if not TOKEN or not CHAT_ID:
+    raise ValueError("❌ TOKEN ou CHAT_ID manquant dans Render.")
