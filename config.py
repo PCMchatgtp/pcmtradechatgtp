5c9b379dc601a43937540e99794f502e6c8ab079
PCMchatgtp <fufumaison@gmail.com>
2025-06-03 20:43:36 +0200

    config.py

--- config.py
+++ config.py
@@ -5,5 +5,7 @@ CHAT_ID = os.getenv("CHAT_ID")
 ALPHA_VANTAGE_API_KEY = os.getenv("ALPHA_VANTAGE_API_KEY")
 NEWS_API_KEY = os.getenv("NEWS_API_KEY")
 
+SYMBOLS = ["XAUUSD", "NASDAQ", "DAX"]
+
 if not TOKEN or not CHAT_ID:
     raise ValueError("❌ TOKEN ou CHAT_ID manquant dans Render.")
