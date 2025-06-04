5a9241cd7d7c4622a7d0144a8a6f9480b5967e52
PCMchatgtp <fufumaison@gmail.com>
2025-06-04 22:49:31 +0200

    config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 TWELVE_API_KEY = os.getenv("TWELVE_API_KEY")
 
 SYMBOLS = {
-    "XAUUSD": "XAU/USD",
-    "BTCUSD": "BTC/USD",
-    "NASDAQ": "QQQ"
+   "XAUUSD": (7, 22),     # Gold : 7h à 22h
+    "BTCUSD": (7, 22),     # BTC/USD : 7h à 22h
+    "NASDAQ": (15.5, 18)   # NASDAQ : 15h30 à 18h
 }
