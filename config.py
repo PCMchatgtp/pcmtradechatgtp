1791ae2f224e78cbb374b84cf75e0568d1baa191
PCMchatgtp <fufumaison@gmail.com>
2025-06-05 22:47:07 +0200

    config.py

--- config.py
+++ config.py
@@ -5,7 +5,13 @@ load_dotenv()
 
 TOKEN = os.getenv("TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
-API_KEY = os.getenv("TWELVE_API_KEY")
+TWELVE_DATA_API_KEY = os.getenv("TWELVE_DATA_API_KEY")
 
-if not all([TOKEN, CHAT_ID, API_KEY]):
+SYMBOLS = {
+    "XAUUSD": os.getenv("SYMBOL_XAUUSD", "XAU/USD"),
+    "BTCUSD": os.getenv("SYMBOL_BTCUSD", "BTC/USD"),
+    "NASDAQ": os.getenv("SYMBOL_NASDAQ", "NDX")
+}
+
+if not all([TOKEN, CHAT_ID, TWELVE_DATA_API_KEY]):
     raise ValueError("❌ Variable d'environnement manquante.")
