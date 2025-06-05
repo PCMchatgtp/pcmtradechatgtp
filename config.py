b150219ed6bd1a3e69840408706401a8bc22a2e8
PCMchatgtp <fufumaison@gmail.com>
2025-06-05 22:57:16 +0200

    config.py

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ load_dotenv()
 
 TOKEN = os.getenv("TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
+API_KEY_OPENAI = os.getenv("API_KEY_OPENAI")
 TWELVE_DATA_API_KEY = os.getenv("TWELVE_DATA_API_KEY")
 
 SYMBOLS = {
