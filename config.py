8664492a5500c2150d3cb073a218325acd61deb8
PCMchatgtp <fufumaison@gmail.com>
2025-06-07 17:09:10 +0200

    Add files via upload

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
 
 TOKEN = os.getenv("TOKEN")
@@ -7,8 +8,23 @@ CHAT_ID = os.getenv("CHAT_ID")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 TWELVE_DATA_API_KEY = os.getenv("TWELVE_DATA_API_KEY")
 
+if not all([TOKEN, CHAT_ID, OPENAI_API_KEY, TWELVE_DATA_API_KEY]):
+    raise ValueError("❌ Variable d'environnement manquante.")
+
 SYMBOLS = {
-    "XAUUSD": "XAU/USD",
-    "BTCUSD": "BTC/USD",
-    "NASDAQ": "QQQ"
+    "XAUUSD": {
+        "symbol": "XAU/USD",
+        "interval": "5min",
+        "actif": "XAUUSD"
+    },
+    "BTCUSD": {
+        "symbol": "BTC/USD",
+        "interval": "5min",
+        "actif": "BTCUSD"
+    },
+    "NASDAQ": {
+        "symbol": "QQQ",
+        "interval": "5min",
+        "actif": "NASDAQ"
+    }
 }
